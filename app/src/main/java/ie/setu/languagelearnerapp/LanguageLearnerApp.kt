ba110354e8beae03ee644fb5aeafa392ca500aef
lauramarin3131 <lauramarin3131@gmail.com>
2025-10-20 13:43:01 +0100

    feat: add main screen with buttons and Timber logging

--- app/src/main/java/ie/setu/languagelearnerapp/LanguageLearnerApp.kt
+++ app/src/main/java/ie/setu/languagelearnerapp/LanguageLearnerApp.kt
@@ -3,18 +3,22 @@ package ie.setu.languagelearnerapp
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+import timber.log.Timber
 
-class MainActivity : AppCompatActivity() {
+class LanguageLearnerApp : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_language_learner)
+        /*
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        */
+        Timber.plant(Timber.DebugTree())
+        Timber.i("LanguageLearner Activity started...")
     }
 }
