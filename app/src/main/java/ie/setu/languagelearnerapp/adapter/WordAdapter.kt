d9d399075dcfa78541a244c08a5682604d40a5f3
lauramarin3131 <lauramarin3131@gmail.com>
2025-10-25 20:22:54 +0200

    feat: Create WordAdapter to display words

--- app/src/main/java/ie/setu/languagelearnerapp/adapter/WordAdapter.kt
+++ app/src/main/java/ie/setu/languagelearnerapp/adapter/WordAdapter.kt
@@ -8,10 +8,14 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import ie.setu.languagelearnerapp.R
 import ie.setu.languagelearnerapp.model.WordModel
+import android.widget.ImageButton
+import androidx.core.content.ContextCompat
 
 class WordAdapter(
         var words: MutableList<WordModel>,
-        private val onDeleteClick: (Int) -> Unit
+        private val onDeleteClick: (Int) -> Unit,
+        private val onEditClick: (WordModel) -> Unit,
+        private val onFavoriteClick: (WordModel) -> Unit
     ) : RecyclerView.Adapter<WordAdapter.WordViewHolder>() {
     fun updateList(newWords: List<WordModel>) {
         words.clear()
@@ -24,6 +28,8 @@ class WordAdapter(
         val tvLanguage: TextView = itemView.findViewById(R.id.tvLanguage)
         val tvLevel: TextView = itemView.findViewById(R.id.tvLevel)
         val btnDelete: Button = itemView.findViewById(R.id.btnDelete)
+        val btnFavorite: ImageButton = itemView.findViewById(R.id.btnFavorite)
+        val btnEdit: Button = itemView.findViewById(R.id.btnEdit)
     }
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): WordViewHolder {
@@ -42,7 +48,17 @@ class WordAdapter(
         holder.btnDelete.setOnClickListener {
             onDeleteClick(position)
         }
+        holder.itemView.setOnClickListener { onEditClick(word) }
+        holder.btnFavorite.setImageResource(
+            if (word.isFavorite) R.drawable.ic_star_filled else R.drawable.ic_star_border
+        )
+        holder.btnFavorite.setColorFilter(ContextCompat.getColor(holder.itemView.context, R.color.colorAccent))
+        holder.btnFavorite.setOnClickListener {
+            onFavoriteClick(word)
+        }
     }
+        override fun getItemCount(): Int = words.size
 
-    override fun getItemCount(): Int = words.size
+
+
 }
