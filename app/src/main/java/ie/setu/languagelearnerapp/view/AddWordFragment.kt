deb2f0bed7212f46782a25f5df6fba850c60c76d
lauramarin3131 <lauramarin3131@gmail.com>
2025-12-16 18:50:41 +0100

    feat: add navigation component with fragment destinations

---
+++ app/src/main/java/ie/setu/languagelearnerapp/view/AddWordFragment.kt
@@ -0,0 +1,6 @@
+package ie.setu.languagelearnerapp.view
+
+import androidx.fragment.app.Fragment
+import ie.setu.languagelearnerapp.R
+class AddWordFragment: Fragment(R.layout.activity_add_word) {
+}
---
+++ app/src/main/java/ie/setu/languagelearnerapp/view/WordListFragment.kt
@@ -0,0 +1,6 @@
+package ie.setu.languagelearnerapp.view
+
+import androidx.fragment.app.Fragment
+import ie.setu.languagelearnerapp.R
+class WordListFragment: Fragment(R.layout.activity_word_list) {
+}
